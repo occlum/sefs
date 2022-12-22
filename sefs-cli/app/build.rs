48123b22af7e05f687a41e38e226fc2a169898f6
volcano0dr <volcano_dr@163.com>
2022-12-22 19:44:25 +0800

    Update rust toolchain to nightly-2022-10-22

--- sefs-cli/app/build.rs
+++ sefs-cli/app/build.rs
@@ -11,22 +11,26 @@ fn main() {
     println!("cargo:rustc-link-lib=static=Enclave_u");
 
     println!("cargo:rustc-link-search=native={}/lib64", sdk_dir);
-    match is_sim.as_ref() {
-        "SW" | "SIM" => println!("cargo:rustc-link-lib=static=sgx_urts_sim_with_se_event"),
-        "HW" => println!("cargo:rustc-link-lib=dylib=sgx_urts"),
-        _ => println!("cargo:rustc-link-lib=dylib=sgx_urts"), // Treat undefined as HW
-    }
-
     // for sgx_tprotected_fs
-    match is_sim.as_ref() {
-        "SW" | "SIM" => println!("cargo:rustc-link-lib=dylib=sgx_uae_service_sim"),
-        "HW" => println!("cargo:rustc-link-lib=dylib=sgx_uae_service"),
-        _ => println!("cargo:rustc-link-lib=dylib=sgx_uae_service"), // Treat undefined as HW
-    }
+    println!("cargo:rustc-link-lib=static=sgx_uprotected_fs");
 
-    println!("cargo:rustc-link-lib=dylib=sgx_uprotected_fs");
-    // Needed by sgx_urts_sim_with_se_event static library
-    println!("cargo:rustc-link-lib=dylib=crypto");
-    // Must link with C++ dynamic library
-    println!("cargo:rustc-link-lib=dylib=stdc++");
+    match is_sim.as_ref() {
+        "SW" | "SIM" => {
+            println!("cargo:rustc-link-arg=-lsgx_urts_sim_with_se_event");
+            println!("cargo:rustc-link-arg=-Wl,-Bdynamic");
+            println!("cargo:rustc-link-arg=-lsgx_uae_service_sim");
+            // Needed by sgx_urts_sim_with_se_event static library
+            println!("cargo:rustc-link-arg=-lcrypto");
+            // Must link with C++ dynamic library
+            println!("cargo:rustc-link-arg=-lstdc++");
+        }
+        "HW" => {
+            println!("cargo:rustc-link-lib=dylib=sgx_urts");
+            println!("cargo:rustc-link-lib=dylib=sgx_uae_service");
+        }
+        _ => {
+            println!("cargo:rustc-link-lib=dylib=sgx_urts");
+            println!("cargo:rustc-link-lib=dylib=sgx_uae_service");
+        } // Treat undefined as HW
+    }
 }
